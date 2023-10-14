aae738d10f06ad9c970ec7add76d6ca702c4aa14
Anderson Elian Gutierrez Bueno <angutierrezb@unal.edu.co>
2023-10-14 00:12:20 -0500

    feat: webhook implementation for continuos deployment

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
+import git
 app = Flask(__name__)
+
+@app.route('/app_update', methods=['POST'])
+def app_update():
+    repo = git.Repo('./FA_T2')
+    origin = repo.remotes.origin
+    repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route("/")
 def home():
